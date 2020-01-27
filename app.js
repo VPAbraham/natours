d5ad9f8b96cbbe838fb4fae8ee0ae36678f449fe
Victor Abraham <vpa4565@gmail.com>
2020-01-27 16:10:09 -0700

    Add endpoints for getting all tours and creating new tours

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
-app.get('/', (req, res) => {
-  res
-  .status(200)
-  .json({ message: 'Hello from the server side!', app: 'Natours' })
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//   .status(200)
+//   .json({ message: 'Hello from the server side!', app: 'Natours' })
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to the endpoint...')
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length ,
+    data: {
+      tours: tours
+    }
+  })
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to the endpoint...')
-});
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
+}) 
 
 const port = 8080;
 app.listen(port, () => {
